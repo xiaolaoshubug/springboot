042824c530a533e0f13ab2ee09fe689d6a87e50a
xiaolaoshubug <aoyio@qq.com>
2021-01-25 16:39:35 +0800

    shiro

---
+++ springboot-14-shiro-demo/src/test/java/com/oay/Springboot14ShiroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot14ShiroDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
